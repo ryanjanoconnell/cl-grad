7242ad491d08cb2878fff588494f13386db1f8a3
Ryan O'Connell <ryanoconnell@Anisoara-s-S21.localdomain>
2023-09-22 11:30:44 +0100

    replaced case statement with lookup table for grad fns

--- cl-grad.lisp
+++ cl-grad.lisp
@@ -6,34 +6,72 @@
 (in-package :cl-grad)
 
 (defclass tensor ()
-  (;; vector of floats
-   (data :initarg :data
-	 :accessor data)
-   ;; list of dims
+  ((buffer :initarg :buffer
+	   :accessor buffer)
    (dims :initarg :dims
-	 :accessor dims)
-   ;; keyword indicating op that produced tensor
+	 :initform nil
+	 :accessor dims)))
+
+(defclass var ()
+  ((tensor :initarg :tensor
+	   :accessor tensor)
    (op :initarg :op
        :initform nil
        :accessor op)
-   ;; list of arguments to the op
    (parents :initarg :parents
 	    :initform nil
 	    :accessor parents)
-   ;; pointer to gradient tensor
    (grad :initarg :grad
 	 :initform nil
 	 :accessor grad)))
 
+;; Table of gradient functions
+(defvar *grad-fns* (make-hash-table))
+
+(defun register-grad-fn (sym fn)
+  (setf (gethash sym *grad-fns*) fn))
+
+(defun get-grad-fn (sym)
+  (let ((fn (gethash sym *grad-fns*)))
+    (if fn
+	fn
+	(assert nil () "No registered grad-fn for op: ~a" sym))))
+
 ;; utils
+(defun copy-tensor (t1)
+  (make-instance
+   'tensor
+   :dims (copy-list (dims t1))
+   :buffer (copy-seq (buffer t1))))
+
+(defun copy-var (v)
+  (make-instance
+   'var
+   :parents (copy-list (parents v))
+   :op (op v)
+   :tensor (copy-tensor (tensor v))
+   :grad (grad v)))
+
+(defun var (dims data)
+  (assert (equal (reduce #'* dims) (length data)))
+  (make-instance
+   'var
+   :tensor (make-instance
+	    'tensor
+	    :dims (copy-list dims)
+	    :buffer (copy-seq data))))
+
+(defmethod print-object ((v var) s)
+  (format s "<Var Dims: ~a Data: ~a>" (dims (tensor v)) (buffer (tensor v))))
+
 (defun same-shape? (t1 t2)
   (equal (dims t1) (dims t2)))
 
-(defun entry-count (t1)
-  (reduce #'* (dims t1)))
-
 (defun matrix? (t1)
   (equal (length (dims t1)) 2))
+
+(defun scalar? (t1)
+  (equal (entry-count t1) 1))
 
 (defun rows (t1)
   (first (dims t1)))
@@ -41,8 +79,8 @@
 (defun cols (t1)
   (second (dims t1)))
 
-(defun scalar? (t1)
-  (null (dims t1)))
+(defun entry-count (t1)
+  (reduce #'* (dims t1)))
 
 (defun can-matmul? (t1 t2)
   (and (matrix? t1)
@@ -60,294 +98,245 @@
     idx))
 
 (defmacro tref (t1 &rest indices)
-  `(aref (data ,t1)
+  `(aref (buffer ,t1)
 	 (linearise-indices (list ,@indices) (dims ,t1))))
 
-(defun copy-tensor (t1)
-  (make-instance 'tensor
-		 :dims (copy-list (dims t1))
-		 :parents (copy-list (parents t1))
-		 :op (op t1)
-		 :grad (grad t1)
-		 :data (copy-seq (data t1))))
+(defun tensor->var (t1)
+  (make-instance 'var :tensor t1))
 
-(defun ones (dims)
-  (make-instance 'tensor
-		 :dims (copy-list dims)
-		 :data
-		 (make-array (reduce #'* dims) :initial-element 1.0)))
-
-
-(defun ten (dims &rest entries)
-  (let ((n (reduce #'* dims)))
-    (assert (equal n (length entries)))
-    (let ((res (make-instance 'tensor
-			      :dims dims
-			      :data (make-array n))))
-      (dotimes (i n res)
-	(setf (aref (data res) i)
-	      (nth i entries))))))
-
-(defmethod print-object ((ten tensor) stream)
-  (format stream
-	  "<~aD-Tensor dims: ~a data: ~a>" (length (dims ten)) (dims ten) (data ten)))
-
-;; entry-wise addition
-(defun add-prepare-child (t1 t2)
-  (assert (same-shape? t1 t2))
-  (make-instance 'tensor
-		 :parents (list t1 t2)
-		 :op :add
-		 :dims (copy-list (dims t1))
-		 :data (make-array (reduce #'* (dims t1)))))
-
-(defun add-compute (t1)
-  (let ((p1 (first (parents t1)))
-	(p2 (second (parents t1))))
-    (dotimes (i (length (data t1)))
-      (setf (aref (data t1) i)
-	    (+ (aref (data p1) i)
-	       (aref (data p2) i)))))
-  t1)
-
-(defun add (t1 t2)
-  (if t1
-      (let ((result (add-prepare-child t1 t2)))
-	(add-compute result))
-      (copy-tensor t2)))
-
-;; entry-wise multiplication
-(defun mul-prepare-child (t1 t2)
-  (assert (same-shape? t1 t2))
-  (make-instance 'tensor
-		 :parents (list t1 t2)
-		 :op :mul
-		 :dims (copy-list (dims t1))
-		 :data (make-array (reduce #'* (dims t1)))))
-
-(defun mul-compute (t1)
-  (destructuring-bind (p1 p2) (parents t1)
-    (dotimes (i (length (data t1)))
-      (setf (aref (data t1) i)
-	    (* (aref (data p1) i) (aref (data p2) i))))
-    t1))
-
-(defun mul (t1 t2)
-  (let ((result (mul-prepare-child t1 t2)))
-    (mul-compute result)))
-
-;; entry-wise subtract
-;; entry-wise multiplication
-(defun sub-prepare-child (t1 t2)
-  (assert (same-shape? t1 t2))
-  (make-instance 'tensor
-		 :parents (list t1 t2)
-		 :op :sub
-		 :dims (copy-list (dims t1))
-		 :data (make-array (reduce #'* (dims t1)))))
-
-(defun sub-compute (t1)
-  (destructuring-bind (p1 p2) (parents t1)
-    (dotimes (i (length (data t1)))
-      (setf (aref (data t1) i)
-	    (- (aref (data p1) i) (aref (data p2) i))))
-    t1))
-
-(defun sub (t1 t2)
-  (let ((result (sub-prepare-child t1 t2)))
-    (sub-compute result)))
-
-;; Matrix multiplication
-(defun matmul-prepare-child (t1 t2)
-  (assert (can-matmul? t1 t2))
-  (make-instance 'tensor
-		 :parents (list t1 t2)
-		 :op :matmul
-		 :dims (list (rows t1) (cols t2))
-		 :data (make-array (* (rows t1) (cols t2)))))
-
-(defun matmul-compute (t1)
-  (let ((p1 (first (parents t1)))
-	(p2 (second (parents t1))))
-    (fill (data t1) 0)
-    (dotimes (i (rows t1))
-      (dotimes (j (cols t1))
-	(dotimes (k (cols p1))
-	  (incf (tref t1 i j)
-		(* (tref p1 i k) (tref p2 k j))))))
-    t1))
-
-(defun matmul (t1 t2)
-  (let ((result (matmul-prepare-child t1 t2)))
-    (matmul-compute result)))
-
-;; scale
+(defun scalar-val (t1)
+  "Unwraps a scalar"
+  (assert (scalar? t1))
+  (aref (buffer t1) 0))
 
 (defun scalar (val)
-  (make-instance 'tensor
-		 :dims nil
-		 :data (make-array 1 :initial-element val)))
+  "Wraps a scalar"
+  (tensor->var (make-instance
+		'tensor
+		:buffer
+		(make-array 1 :initial-element val))))
 
-(defun scale (t1 t2)
+
+;; Generators
+(defun t-ones (dims)
+  (make-instance
+   'tensor
+   :dims (copy-list dims)
+   :buffer
+   (make-array (reduce #'* dims) :initial-element 1.0)))
+
+(defun ones (dims)
+  (tensor->var (t-ones dims)))
+
+;; Elementwise function makers
+(defun make-tensor-uop (uop)
+  (lambda (t1)
+    (let ((result (copy-tensor t1)))
+      (dotimes (i (length (buffer result)) result)
+	(setf (aref (buffer result) i)
+	      (funcall uop (aref (buffer result) i)))))))
+
+(defun make-tensor-bop (bop)
+  (lambda (t1 t2)
+    (assert (same-shape? t1 t2))
+    (let ((result
+	    (make-instance
+	     'tensor
+	     :dims (copy-list (dims t1))
+	     :buffer (make-array (reduce #'* (dims t1))))))
+      (dotimes (i (length (buffer result)) result)
+	(setf (aref (buffer result) i)
+	      (funcall bop (aref (buffer t1) i) (aref (buffer t2) i)))))))
+
+;; Lift a tensor-fn to a var-fn
+(defun wrap-tensor-fn (name t-fn)
+  (setf (symbol-function name) 
+	(lambda (&rest vars)
+	  (let ((ts (mapcar #'tensor vars)))
+	    (make-instance
+	     'var
+	     :parents vars
+	     :op name
+	     :tensor (apply t-fn ts))))))
+
+;; Scale
+(defun t-scale (t1 t2)
   (assert (scalar? t1))
   (let ((result (copy-tensor t2)))
-    (dotimes (i (length (data result)) result)
-      (setf (aref (data result) i)
-	    (* (aref (data result) i)
-	       (aref (data t1) 0))))))
+    (dotimes (i (length (buffer result)) result)
+      (setf (aref (buffer result) i)
+	    (* (scalar-val t1)
+	       (aref (buffer result) i))))))
 
-(defun div (t1 t2)
-  (assert (scalar? t1))
-  (let ((result (copy-tensor t2)))
-    (dotimes (i (length (data result)) result)
-      (setf (aref (data result) i)
-	    (/ (aref (data result) i)
-	       (aref (data t1) 0))))))
+(wrap-tensor-fn 'scale #'t-scale)
 
-(defun translate (t1 t2)
-  (assert (scalar? t1))
-  (let ((result (copy-tensor t2)))
-    (dotimes (i (length (data result)) result)
-      (setf (aref (data result) i)
-	    (+ (aref (data result) i)
-	       (aref (data t1) 0))))))
+;; Add
+(setf (symbol-function 't-add)
+      (make-tensor-bop #'+))
+
+(wrap-tensor-fn 'add #'t-add)
+
+(defun addn (v1 v2)
+  (cond
+    ((null v1) (copy-var v2))
+    ((null v2) (copy-var v1))
+    ((and v1 v2) (add v1 v2))
+    (t (assert nil () "addn must have at least one non-nil arg"))))
+
+(defmacro add-setf (t1 t2)
+  `(setf ,t1 (addn ,t1 ,t2)))
+
+(defun add-grad (v)
+  (destructuring-bind (p1 p2) (parents v)
+    (add-setf (grad p1) (grad v))
+    (add-setf (grad p2) (grad v))))
+
+(register-grad-fn 'add #'add-grad)
+
+;; Subtract
+(setf (symbol-function 't-sub)
+      (make-tensor-bop #'-))
+
+(wrap-tensor-fn 'sub #'t-sub)
+
+;; Multiplication
+(setf (symbol-function 't-mul)
+      (make-tensor-bop #'*))
+
+(wrap-tensor-fn 'mul #'t-mul)
+
+;; Division
+(setf (symbol-function 't-div)
+      (make-tensor-bop #'/))
+
+(wrap-tensor-fn 'div #'t-div)
+
+;; Transpose
+(defun t-transpose (t1)
+  (assert (matrix? t1))
+  (let ((result (make-instance
+		 'tensor
+		 :dims (list (cols t1) (rows t1))
+		 :buffer (make-array (* (cols t1) (rows t1))))))
+    (dotimes (i (rows result) result)
+      (dotimes (j (cols result))
+	(setf (tref result i j) (tref t1 j i))))))
+
+(wrap-tensor-fn 'transpose #'t-transpose)
+
+;; Matmul
+(defun t-matmul (t1 t2)
+  (assert (can-matmul? t1 t2))
+  (let ((result (make-instance
+		 'tensor
+		 :dims (list (rows t1) (cols t2))
+		 :buffer (make-array (* (rows t1) (cols t2))
+				     :initial-element 0.0))))
+    (dotimes (i (rows result) result)
+      (dotimes (j (cols result))
+	(dotimes (k (cols t1))
+	  (incf (tref result i j)
+		(* (tref t1 i k) (tref t2 k j))))))))
+
+(wrap-tensor-fn 'matmul #'t-matmul)
+
+(defun matmul-grad (v)
+  (destructuring-bind (p1 p2) (parents v)
+    (add-setf (grad p1)
+	      (matmul (grad v) (transpose p2)))    
+    (add-setf (grad p2)
+	      (matmul (transpose p1) (grad v)))))
+
+(register-grad-fn 'matmul #'matmul-grad)
 
 ;; Sigmoid
-(defun sigmoid (x)
+(defun sig (x)
   (/ 1 (+ 1 (exp (- x)))))
 
-(defun sig-prepare-child (t1)
-  (make-instance 'tensor
-		 :parents (list t1)
-		 :op :sig
-		 :dims (copy-list (dims t1))
-		 :data (make-array (reduce #'* (dims t1)))))
+(setf (symbol-function 't-sigmoid)
+      (make-tensor-uop #'sig))
 
-(defun sig-compute (t1)
-  (let ((p1 (first (parents t1))))
-    (dotimes (i (length (data t1)))
-      (setf (aref (data t1) i)
-	    (sigmoid (aref (data p1) i))))
-    t1))
+(wrap-tensor-fn 'sigmoid #'t-sigmoid)
 
-(defun sig (t1)
-  (let ((result (sig-prepare-child t1)))
-    (sig-compute result)))
+(defun dsig (v)
+  (let ((ones (ones (dims (tensor v)))))
+    (mul v (sub ones v))))
 
-(defun dsig (t1)
-  (mul t1 (sub (ones (dims t1)) t1)))
+(defun sigmoid-grad (v)
+  (destructuring-bind (p) (parents v)
+    (add-setf (grad p)
+	      (mul (grad v) (dsig v)))))
 
-;; Mean Squared Error
-(defun mse-prepare-child (t1 t2)
+(register-grad-fn 'sigmoid #'sigmoid-grad)
+
+;; MSE loss
+(defun t-mse (t1 t2)
   (assert (same-shape? t1 t2))
-  (make-instance 'tensor
-		 :parents (list t1 t2)
-		 :op :mse
-		 :dims nil
-		 :data (make-array 1)))
-
-(defun mse-compute (t1)
-  (let* ((p1 (first (parents t1)))
-	 (p2 (second (parents t1)))
-	 (n (length (data p1)))
-	 (acc 0))
+  (let ((result (make-instance
+		 'tensor
+		 :buffer
+		 (make-array 1)))
+	(n (entry-count t1))
+	(acc 0.0))
     (dotimes (i n)
-      (incf acc (expt (- (aref (data p1) i) (aref (data p2) i)) 2)))
-    (setf (aref (data t1) 0) (/ acc n))
-    t1))
+      (incf acc
+	    (expt (- (aref (buffer t1) i)
+		     (aref (buffer t2) i))
+		  2)))
+    (setf (aref (buffer result) 0)
+	  (/ acc n))
+    result))
 
-(defun mse (t1 t2)
-  (let ((result (mse-prepare-child t1 t2)))
-    (mse-compute result)))
+(wrap-tensor-fn 'mse #'t-mse)
 
-;; Matrix transpose
-(defun transpose-prepare-child (t1)
-  (assert (matrix? t1))
-  (make-instance 'tensor
-		 :parents (list t1)
-		 :op :transpose
-		 :dims (list (cols t1) (rows t1))
-		 :data (make-array (* (cols t1) (rows t1)))))
+(defun mse-grad (v)
+  (destructuring-bind (p1 p2) (parents v)
+    (add-setf (grad p1)
+	      (scale (mul (grad v) (scalar (/ 1 (entry-count (tensor v)))))
+		     (sub p1 p2)))
+    (add-setf (grad p2)
+	      (scale (mul (grad v) (scalar (/ 1 (entry-count (tensor v)))))
+		     (sub p2 p1)))))
 
-(defun transpose-compute (t1)
-  (let ((p1 (first (parents t1))))
-    (dotimes (i (rows p1))
-      (dotimes (j (cols p1))
-	(setf (tref t1 j i) (tref p1 i j))))
-    t1))
-
-(defun transpose (t1)
-  (let ((result (transpose-prepare-child t1)))
-    (transpose-compute result)))
+(register-grad-fn 'mse #'mse-grad)
 
 ;; Backprop
-(defun compute-parent-grads (t1)
-  (destructuring-bind (p1 &optional p2) (parents t1)
-    (ecase (op t1)
-      (:add
-       (setf (grad p1)
-	     (add (grad p1) (grad t1)))
-       (setf (grad p2)
-	     (add (grad p2) (grad t1))))
-      
-      (:matmul
-       (setf (grad p1)
-	     (add (grad p1) (matmul (grad t1) (transpose p2))))
-       (setf (grad p2)
-	     (add (grad p2) (matmul (transpose p1) (grad t1)))))
-      
-      (:sig
-       (setf (grad p1)
-	     (add (grad p1) (mul (grad t1) (dsig t1)))))
-      
-      (:mse
-       (setf (grad p1)
-	     (add (grad p1)
-		  (scale (mul (grad t1) (scalar (/ 2 (entry-count p1))))
-			 (sub p1 p2))))
-       (setf (grad p2)
-	     (add (grad p2)
-		  (scale (mul (grad t1) (scalar (/ 2 (entry-count p2))))
-			 (sub p2 p1))))))))
+(defun compute-parent-grads (v)
+  (funcall (get-grad-fn (op v)) v))
 
-(defun backprop (t1)
-  (assert (scalar? t1))
-  (setf (grad t1) (scalar 1.0))
-  (labels ((recur (tn)
-	     (when (parents tn)
-	       (compute-parent-grads tn)
-	       (mapc #'recur (parents tn)))))
-    (recur t1)))
+(defun backward (v)
+  (assert (scalar? (tensor v)))
+  (setf (grad v) (scalar 1.0))
+  (labels ((recur (var)
+	     (when (parents var)
+	       (compute-parent-grads var)
+	       (mapc #'recur (parents var)))))
+    (recur v)
+    nil))
+
+;;;;; TEST TIME!!!! ;;;;;
+;;;;; This outputs the same as pytorch ;;;;;;
+
+;; (let* ((w (var '(2 2) #(0.1 0.2 0.3 0.4)))
+;;        (b (var '(2 1) #(0.2 0.3)))
+;;        (x (var '(2 1) #(0.5 0.1)))
+;;        (y (var '(2 1) #(0.1 0.2)))
+;;        (affine (add (matmul w x) b))
+;;        (sig (sigmoid affine))
+;;        (cost (mse sig y)))
+;;   (backward cost)
+;;   (format t "Gradients: ~%")
+;;   (format t "Dw = ~a~%" (grad w))
+;;   (format t "Db = ~a~%" (grad b))
+;;   (format t "Dx = ~a~%" (grad x))
+;;   (format t "Dy = ~a~%" (grad y)))
 
 
-;; sanity tests, comparing outputs to pytorch
-;; it works for this example
 
-;; (let*
-;;     ((w (ten '(2 2)
-;; 	     0.1 0.2
-;; 	     0.3 0.4))
-       
-;;      (b (ten '(2 1)
-;; 	     0.2
-;; 	     0.3))
-       
-;;      (x (ten '(2 1)
-;; 	     0.5
-;; 	     0.1))
-       
-;;      (y (ten '(2 1)
-;; 	     0.1
-;; 	     0.2))
-     
-;;      (affine (add (matmul w x) b))
-;;      (sig (sig affine))
-;;      (c (mse sig y)))
-;;   (backprop c)
-;;   (format t "~%Gadients:~%~%")
-;;   (format t "dc/dc = ~a~%~%" (grad c))
-;;   (format t "dc/dy = ~a~%~%" (grad y))
-;;   (format t "dc/dw = ~a~%~%" (grad w))
-;;   (format t "dc/db = ~a~%~%" (grad b))
-;;   (format t "dc/dx = ~a~%~%" (grad x)))
+
+
+
+
+
+
 
